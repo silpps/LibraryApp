ea2a4bcfbd9bd77c16b5982b2334430ec996257c
vevego <veera.ruotsalainen@metropolia.fi>
2024-12-09 22:39:23 +0200

    deployment kinda works but backend doesnt connect. there are maybe fixing it??

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,17 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
-
 export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: process.env.VITE_API_URL || 'http://localhost:4000',
         changeOrigin: true,
       },
-    }
+    },
   },
-})
+});
