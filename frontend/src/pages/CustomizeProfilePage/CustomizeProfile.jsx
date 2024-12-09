0146bb7ac2ef4c8e3832ac477b26d717324062ec
deardreamyy <karoi@metropolia.fi>
2024-12-09 16:31:31 +0200

    responsive css to settings

--- frontend/src/pages/CustomizeProfilePage/CustomizeProfile.jsx
+++ frontend/src/pages/CustomizeProfilePage/CustomizeProfile.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import { useProfile } from '../../context/ProfileContext';  
 import { useNavigate, useLocation } from 'react-router-dom';  
 import './CustomizeProfile.css';  
@@ -10,9 +10,14 @@ import pfp3 from '../../assets/bh_pfp_3.jpg';
 const CustomizeProfile = () => {
   const location = useLocation();
   const { profilePicture, username, description, updateProfile } = useProfile(); 
-  const [newDescription, setNewDescription] = useState(description);
-  const [newProfilePicture, setNewProfilePicture] = useState(profilePicture);
+  const [newDescription, setNewDescription] = useState('');
+  const [newProfilePicture, setNewProfilePicture] = useState('');
   const navigate = useNavigate();
+
+  useEffect(() => {
+    setNewDescription(description);
+    setNewProfilePicture(profilePicture);
+  }, [description, profilePicture]);
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -27,69 +32,83 @@ const CustomizeProfile = () => {
   };
 
   return (
-    <div className="customize-profile-container">
-      {location.pathname === '/settings' && <h1>Edit Profile</h1>}
-      {location.pathname === '/customize-profile' && (
-        <>
-        <h1>Customize Profile</h1>
-        <p>Thank you for signing up to BookHive! </p>
-        <p>Let's get started by customizing your profile.</p>
-        </>
-      )}
-      <form onSubmit={handleSubmit}>
-        <label>Description: </label>
-        {location.pathname === '/settings' && (
-          <textarea
-          placeholder={description}
-          value={newDescription}
-          onChange={(e) => setNewDescription(e.target.value)}
-        />
-        )}
+    <div className="customize-profile-wrapper">
+      <div className="customize-profile-container">
+        {location.pathname === '/settings' && <h1>Edit Profile</h1>}
         {location.pathname === '/customize-profile' && (
-          <textarea
-          placeholder={`Tell us about yourself, ${username}!`}
-          value={newDescription}
-          onChange={(e) => setNewDescription(e.target.value)}
-        />
+          <>
+          <h1>Customize Profile</h1>
+          <p className="info-text">Thank you for signing up to BookHive! </p>
+          <p className="info-text">Let's get started by customizing your profile.</p>
+          </>
         )}
-        <div className="profile-pic-options">
-          <label>
-            Profile picture:
-            <input
-              type="radio"
-              name="profilePicture"
-              value="bh_pfp_1"
-              checked={newProfilePicture === 'bh_pfp_1'}
-              onChange={() => setNewProfilePicture('bh_pfp_1')}
-            />
-            <img src={pfp1} alt="Profile 1" className="profile-pic-option"/>
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="profilePicture"
-              value="bh_pfp_2"
-              checked={newProfilePicture === 'bh_pfp_2'}
-              onChange={() => setNewProfilePicture('bh_pfp_2')}
-            />
-            <img src={pfp2} alt="Profile 2" className="profile-pic-option"/>
-          </label>
-          <label>
-            <input
-              type="radio"
-              name="profilePicture"
-              value="bh_pfp_3"
-              checked={newProfilePicture === 'bh_pfp_3'}
-              onChange={() => setNewProfilePicture('bh_pfp_3')}
-            />
-            <img src={pfp3} alt="Profile 3" className="profile-pic-option"/>
-          </label>
-        </div>
-        <button type="submit">Save Changes</button>
-        {location.pathname === '/settings' && (
-          <button onClick={cancelHandler}>Cancel</button>
-        )}
-      </form>
+        <form onSubmit={handleSubmit}>
+          {location.pathname === '/settings' && (
+            <>
+            <h3>Description: </h3>
+              <textarea
+                placeholder={description}
+                value={newDescription}
+                onChange={(e) => setNewDescription(e.target.value)}
+                maxLength="60"
+                rows="2"
+              />
+              <p className="characters-text">{60 - newDescription.length} characters remaining</p>
+              <h3>Profile picture: </h3>
+            </>
+          )}
+          {location.pathname === '/customize-profile' && (
+            <>
+            <h3>Write a description: </h3>
+              <textarea
+                placeholder={`Tell us about yourself, ${username}!`}
+                value={newDescription}
+                onChange={(e) => setNewDescription(e.target.value)}
+                maxLength="60"
+                rows="2"
+              />
+              <p className="characters-text">{60 - newDescription.length} characters remaining</p>
+              <h3>Choose a profile picture:</h3>
+            </>
+          )}
+          <div className="profile-pic-options">
+            <label>
+              <input
+                type="radio"
+                name="profilePicture"
+                value="bh_pfp_1"
+                checked={newProfilePicture === 'bh_pfp_1'}
+                onChange={() => setNewProfilePicture('bh_pfp_1')}
+              />
+              <img src={pfp1} alt="Profile 1" className="profile-pic-option"/>
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="profilePicture"
+                value="bh_pfp_2"
+                checked={newProfilePicture === 'bh_pfp_2'}
+                onChange={() => setNewProfilePicture('bh_pfp_2')}
+              />
+              <img src={pfp2} alt="Profile 2" className="profile-pic-option"/>
+            </label>
+            <label>
+              <input
+                type="radio"
+                name="profilePicture"
+                value="bh_pfp_3"
+                checked={newProfilePicture === 'bh_pfp_3'}
+                onChange={() => setNewProfilePicture('bh_pfp_3')}
+              />
+              <img src={pfp3} alt="Profile 3" className="profile-pic-option"/>
+            </label>
+          </div>
+          <div className="button-container">
+            <button type="submit">Save</button>
+            <button type="button" onClick={cancelHandler}>Cancel</button>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
