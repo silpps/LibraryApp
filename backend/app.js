be255a395302090e537058995bf149beb223fea0
vevego <veera.ruotsalainen@metropolia.fi>
2024-12-10 21:04:44 +0200

    added open api/ swagger

--- backend/app.js
+++ backend/app.js
@@ -5,6 +5,9 @@ const express = require("express");
 const app = express();
 const userRouter = require('./routers/userRouter');
 const bookRouter = require('./routers/bookRouter');
+const { unknownEndpoint,errorHandler } = require("./middleware/customMiddleware");
+const swaggerUI = require('swagger-ui-express');
+const swaggerSpec = require('./swagger.json');
 const cors = require("cors");
 
 const morgan = require("morgan")
@@ -18,6 +21,10 @@ app.use(express.json());
 app.use('/api/users', userRouter);
 //the book path is for now called library. consider changing paths again when adding wishlist and readinglist
 app.use('/api/library', bookRouter);
+
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerSpec));
+app.use(unknownEndpoint);
+app.use(errorHandler);
 
 module.exports = app;
 // // Start the server
