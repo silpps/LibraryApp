105c8c1a7eeb8c8240e2e7f4d2aca440ad238837
silpps <hilda.hermunen@gmail.com>
2024-12-10 21:34:15 +0200

    Custom hook for change password

--- frontend/src/pages/ChangePassword/ChangePassword.jsx
+++ frontend/src/pages/ChangePassword/ChangePassword.jsx
@@ -1,83 +1,28 @@
-import React, { useState } from 'react';
-import { REACT_APP_API_URL } from '../../utils/apiConfig';
-import { useNavigate } from 'react-router-dom';
+// src/pages/ChangePasswordPage/ChangePassword.js
+
+import React from 'react';
+import useChangePassword from '../../hooks/useChangePassword';
 import '../../pages/SignUpPage/SignUp.css';
 
-const apiUrl = `${REACT_APP_API_URL}`;
-
 const ChangePassword = () => {
-  const [currentPassword, setCurrentPassword] = useState('');
-  const [newPassword, setNewPassword] = useState('');
-  const [confirmPassword, setConfirmPassword] = useState('');
-  const [message, setMessage] = useState('');
-  const [error, setError] = useState('');
-  const [passwordStrengthError, setPasswordStrengthError] = useState(''); // Error for password strength
-  const navigate = useNavigate();
-
-  // Validate password strength (must include lowercase, uppercase, numbers, special characters, and be at least 8 characters long)
-  const validatePassword = (password) => {
-    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[\W_])[\S]{8,}$/;
-    return passwordRegex.test(password);
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    setMessage('');
-    setError('');
-    setPasswordStrengthError('');
-
-    if (newPassword !== confirmPassword) {
-      setError('New passwords do not match.');
-      return;
-    }
-
-    // Check if new password is strong
-    if (!validatePassword(newPassword)) {
-      setPasswordStrengthError('Password must be at least 8 characters long and contain uppercase, lowercase, number, and special character.');
-      return;
-    }
-
-    // Get user data and token from localStorage
-    const userDataString = localStorage.getItem('userData');
-    if (!userDataString) {
-      setError('User data not found. Please log in again.');
-      navigate('/login');
-      return;
-    }
-
-    const userData = JSON.parse(userDataString);
-    const token = userData.token;
-
-    try {
-      const res = await fetch(`${apiUrl}/users/change-password`, {
-        method: 'PUT',
-        headers: {
-          'Content-Type': 'application/json',
-          'Authorization': `Bearer ${token}`,
-        },
-        body: JSON.stringify({ currentPassword, newPassword }),
-      });
-
-      const data = await res.json();
-
-      if (res.status === 200) {
-        setMessage(data.message || 'Password changed successfully!');
-        setTimeout(() => {
-          navigate('/profile'); 
-        }, 2000);
-      } else {
-        throw new Error(data.error || 'Failed to change password.');
-      }
-    } catch (err) {
-      setError(err.message);
-    }
-  };
+  const {
+    currentPassword,
+    setCurrentPassword,
+    newPassword,
+    setNewPassword,
+    confirmPassword,
+    setConfirmPassword,
+    message,
+    error,
+    passwordStrengthError,
+    handleSubmit,
+  } = useChangePassword();
 
   return (
     <div className="container">
       <h1>Change Password</h1>
       <form onSubmit={handleSubmit}>
-        <label htmlFor="currentPassword">Current Password:</label>
+        <label className="currentPassword">Current Password:</label>
         <input
           type="password"
           id="currentPassword"
@@ -85,7 +30,7 @@ const ChangePassword = () => {
           onChange={(e) => setCurrentPassword(e.target.value)}
           required
         />
-        <label htmlFor="newPassword">New Password:</label>
+        <label className="newPassword">New Password:</label>
         <input
           type="password"
           id="newPassword"
@@ -93,8 +38,8 @@ const ChangePassword = () => {
           onChange={(e) => setNewPassword(e.target.value)}
           required
         />
-        {passwordStrengthError && <p className="error-message">{passwordStrengthError}</p>} {/* Show strength error */}
-        <label htmlFor="confirmPassword">Confirm New Password:</label>
+        {passwordStrengthError && <p className="error-message">{passwordStrengthError}</p>}
+        <label className="confirmPassword">Confirm New Password:</label>
         <input
           type="password"
           id="confirmPassword"
